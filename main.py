ab1aaca6ef03c448f102932089906326b3741018
ggers <sergey.prosol@gmail.com>
2018-08-31 20:02:58 +0300

    final attempt

--- main.py
+++ main.py
@@ -19,7 +19,6 @@ id1 = 13575261
 id2 = 171691064
 #получение токена
 print("?".join((AUTH_SERVER, urlencode(auth_data))))
-#TOKEN = "203d08492f2160e55c9af9a7587fec06666678eefe35ad258ac0e82b1cb64fe8de3e5aaa8b4c6528cb178"
 TOKEN = "99fb9a4f034dc329f4d86efb6bfdae66452be73b4bdecc6ebf8449796ef73370dc165de4640f64618bebd"
 
 
@@ -27,7 +26,7 @@ TOKEN = "99fb9a4f034dc329f4d86efb6bfdae66452be73b4bdecc6ebf8449796ef73370dc165de
 def get_VKfriends_by_id(VK_token, user_id):
     response = requests.get("https://api.vk.com/method/friends.get",
                             params=dict(access_token=VK_token, v=5.80, user_ids=user_id))
-    print(f"У пользователя {user_id} обнаружилось целых {response.json()['response']['count']} друзей")
+    print(f"User number {user_id} have total {response.json()['response']['count']} friends")
     return response.json()["response"]['items']
 
 #запрашиваем список групп
@@ -38,14 +37,14 @@ def get_VKgroups_by_id(VK_token, user_id):
         x = response.json()['response']['items']
         return x
     except:
-        print(f"Спешу уведомить, что ваш друг {user_id} от вас что-то скрывает!")
+        print(f"Have no access to user {user_id}. He hides something from you...")
         return None
 
 #упрощенное решение задачи для пользователя связанного с токеном
 def get_friends_in_group(VK_token, group):
     users_in_group = requests.get("https://api.vk.com/method/groups.getMembers",
                             params=dict(access_token=VK_token, v=5.80, group_id=group, filter="friends"))
-    print(f"В группе {group} обнаружилось {users_in_group.json()['response']['count']} друзей\n")
+    print(f"Target group {group} consists {users_in_group.json()['response']['count']} friends\n")
     return users_in_group.json()['response']['count']
 
 #запрашиваем информацию о группе по её идентификатору
@@ -63,42 +62,36 @@ def get_group_info_by_id(VK_token, list_group):
     print(result_json)
     return result_json
 
-def difficult_way():
-    list_friends = get_VKfriends_by_id(TOKEN, 171691064)
-    set_goups = set(get_VKgroups_by_id(TOKEN, 171691064))
-
-    # print(list_friends)
-    # print(set_goups)
+def difficult_way(id):
+    list_friends = get_VKfriends_by_id(TOKEN, id)
+    set_goups = set(get_VKgroups_by_id(TOKEN, id))
     friend_set = set_goups
     result = set_goups
-    print(f"Сначала было {len(result)} элементов:\n  {result}")
+    print(f"Firstly we have {len(result)} elements:\n  {result}")
 
     for friend in list_friends:
-        print(f"Обрабатываем друга {friend}")
+        print(f"Checking friend number {friend}")
         now = get_VKgroups_by_id(TOKEN, friend)
         if now:
             friend_set = set(now)
-        #        print(friend_set)
         sleep(0.28)
         result.difference_update(friend_set)
-        print(f"После этого цикла осталось {len(result)} элементов:\n")
-    print(f"В конце порлучилось {len(result)} элементов:\n  {result}")
+        print(f"Now we have only {len(result)} secret groups:\n")
+    print(f"Finally we have {len(result)} elements:\n  {result}")
     return result
 
-def easy_way():
-
-    groups_total = get_VKgroups_by_id(TOKEN, 171691064)
+def easy_way(id):
+    groups_total = get_VKgroups_by_id(TOKEN, id)
     result = list()
     for group_id in groups_total:
         if not get_friends_in_group(TOKEN, group_id):
-            print(f"Группа {group_id} нам подходит \n")
+            print(f"Group {group_id} is for your eyes only.\n")
             result.append(group_id)
         sleep(0.28)
     print(result)
     return result
 
-def collect_data (result):
-
+def collect_data(result):
     result_json = list()
     for i in result:
         tmp_dict = dict()
@@ -110,25 +103,42 @@ def collect_data (result):
         tmp_dict["members_count"] = response.json()['response'][0]['members_count']
 #        print(tmp_dict)
         result_json.append(tmp_dict)
+        sleep(0.28)
     print(result_json)
     return result_json
 
+def export_data_to_file(filename, data):
+    with open(filename, 'w', encoding="utf-8") as outfile:
+        json.dump(data, outfile)
+
+def get_user_id():
+    while True:
+        user_id = input(" > ")
+        if user_id.isdigit():
+            print(f"Initialising spy action for user {user_id}")
+            break
+        else:
+            print(f"ERROR!")
+    return user_id
+
+def validate_main_user(VK_token, user_id):
+    response = requests.get("https://api.vk.com/method/users.isAppUser",
+                            params=dict(access_token=VK_token, v=5.80, user_id=user_id))
+    return response.json()['response']
 
 if __name__ == '__main__':
-    print(f"Добро пожаловать в Скайнет v. 0.311")
-    print(f"В настоящее время получен доступ к данным социальной сети VK.\n ")
-    print(f"Вы можете ввести свой идентификатор, либо чужой и узнать, в каких группах состоит этот пользователь без своих друзей\n ")
-    user_id = input(" > ")
-    i = difficult_way()
-    collect_data(i)
-
-
-
-
-
-
-    #
-    # with open('groups.json', 'w', encoding="utf-8") as outfile:
-    #     json.dump(result_json, outfile)
-
+    print(f"Welcome to SkyNet v. 0.311\n")
+    print(f"Now we have access to social net VK.\n Input your id")
+    i = get_user_id()
+    if validate_main_user(TOKEN, i):
+        print(f"YOUR Account secrets you can see in 'secret.json' file\n")
+        export_data_to_file('secret.json', collect_data(easy_way(i)))
+    else:
+        print(f"Access denied!\n")
+    print(f"Input user id to access this user secret groups\n")
+    j = get_user_id()
+    if get_VKgroups_by_id(TOKEN, j):
+        export_data_to_file('groups.json', collect_data(difficult_way(j)))
+    else:
+        print(f"Access denied!\n")
 
